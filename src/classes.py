8badbd35547a26398dd148b23f9e7cc38e67c833
Luka Buyanov <luka.buanov@gmail.com>
2024-10-15 16:14:37 +0300

    Создана инициализация в классах. В классе Category созданы атрибуты подсчёта количества категорий и продуктов в них

--- src/classes.py
+++ src/classes.py
@@ -4,8 +4,23 @@ class Product:
     price: str
     quantity: int
 
+    def __init__ (self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
 
 class Category:
     name: str
     description: str
     products: list[object]
+
+    category_count = 0
+    products_count = 0
+
+    def __init__(self, name, description, products, category_count, products_count):
+        self.name = name
+        self.description = description
+        self.products = products
+        category_count += 1
+        products_count += len(products)
